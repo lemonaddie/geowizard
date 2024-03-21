9ae264816c6f0631a91ac59e124aaebb840671b9
XiaoFu <lemonaddie0909@gmail.com>
2024-03-22 02:28:48 +0800

    update

--- pipeline/models/unet_2d_blocks.py
+++ pipeline/models/unet_2d_blocks.py
@@ -28,7 +28,7 @@ from diffusers.models.attention_processor import Attention, AttnAddedKVProcessor
 from diffusers.models.dual_transformer_2d import DualTransformer2DModel
 from diffusers.models.normalization import AdaGroupNorm
 from diffusers.models.resnet import Downsample2D, FirDownsample2D, FirUpsample2D, KDownsample2D, KUpsample2D, ResnetBlock2D, Upsample2D
-from .transformer_2d import Transformer2DModel
+from models.transformer_2d import Transformer2DModel
 
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
